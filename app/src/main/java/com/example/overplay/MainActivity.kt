87efb95eb4466f45dbadd07a3aaf16a3ddc38d05
DiegoU <diego.urrea@thriveglobal.com>
2022-04-05 10:32:51 -0500

    Overlay: Added view binding delegate helper

--- app/src/main/java/com/example/overplay/MainActivity.kt
+++ app/src/main/java/com/example/overplay/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.overplay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.overplay.databinding.ActivityMainBinding
+import com.example.overplay.utils.viewBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
