1d9713344493b3a4ac27249d2f71531c4490e690
DiegoU <diego.urrea@thriveglobal.com>
2022-04-05 14:08:45 -0500

    Overlay: Implemented basic mp4 ExoPlayer

--- app/src/main/java/com/example/overplay/MainActivity.kt
+++ app/src/main/java/com/example/overplay/MainActivity.kt
@@ -1,16 +1,78 @@
 package com.example.overplay
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.overplay.databinding.ActivityMainBinding
-import com.example.overplay.utils.viewBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.util.Util
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding by viewBinding(ActivityMainBinding::inflate)
+    private lateinit var binding: ActivityMainBinding
+
+    private var exoPlayer: ExoPlayer? = null
+
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT <= 23 || exoPlayer == null) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun initializePlayer() {
+        val videoUrl =
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
+
+        exoPlayer = ExoPlayer.Builder(this)
+            .build()
+            .also { exoPlayer ->
+                binding.exoPlayerView.player = exoPlayer
+                exoPlayer.setMediaItem(MediaItem.fromUri(videoUrl))
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(currentWindow, playbackPosition)
+                exoPlayer.prepare()
+            }
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.run {
+            playbackPosition = this.currentPosition
+            currentWindow = this.currentWindowIndex
+            playWhenReady = this.playWhenReady
+            release()
+        }
+        exoPlayer = null
     }
 }
